35ca8cf52e7a943309b66c62830610d8cd6647a1
Ovi Stoica <ost@3e.eu>
2023-06-19 08:59:36 +0300

    Initial commit

---
+++ test/os/learn_core_async_test.clj
@@ -0,0 +1,7 @@
+(ns os.learn-core-async-test
+  (:require [clojure.test :refer :all]
+            [os.learn-core-async :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
